f9dfabd6b8264e574190d812b9598dcae03e3eba
Phil <happy.melodist@gmail.com>
2021-10-18 22:15:28 -0500

    complete JavaScript behaviors for section wheel scroll effect

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
 const happyDay = document.querySelector(".happy-day");
 const thisIsTheNewYear = new Date().getFullYear();
 happyDay.textContent = thisIsTheNewYear;
+
+let counter1 = 0;
+let counter2 = 1;
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("wheel", (e) => {
+  const deltaDawn = e.deltaY > 0;
+
+  if (deltaDawn) {
+    counter1++;
+    counter2++;
+  } else {
+    counter1--;
+    counter2--;
+  }
+
+  if (counter1 === 5) {
+    Array.from(sections).forEach((section) => {
+      section.style.left = "0";
+    });
+    counter1 = 0;
+    counter2 = 1;
+    return;
+  }
+
+  if (counter1 === -1) {
+    Array.from(sections).forEach((section) => {
+      if (section.classList[0] === "section-5") {
+        return;
+      }
+      section.style.left = "-100vw";
+    });
+    counter1 = 4;
+    counter2 = 5;
+  }
+
+  document.querySelector(
+    `.section-${deltaDawn ? counter1 : counter2}`
+  ).style.left = `${deltaDawn ? "-100vw" : "0"}`;
+
+  console.log(counter1, counter2);
+});
